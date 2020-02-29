80fe0b46b9962bfe101152ba2594d23d8266717d
Seda-Civan <sedademircivan@gmail.com>
2020-02-29 15:26:33 -0500

    modified StringPractice class

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,6 +5,9 @@ public class StringPractice {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-
+        String str = "Java is fun";
+        System.out.println(str);
     }
 }
+
+
